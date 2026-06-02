a97e97c183245be054fea141ced6d04169dcd7cb
unknown <jhongilb1993@outlook.com>
2026-06-02 15:56:49 -0500

    Configurar base para GitHub Pages

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    tailwindcss(),
-  ],
-})
+  plugins: [vue()],
+  base: '/Portafolio/', // nombre EXACTO de tu repo [web:126][web:111]
+});
